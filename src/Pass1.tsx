ec4d616aff2b8bd26b3460b4635ad9057087e63b
Gianni Crivello <gianniemperado@gmail.com>
2021-02-24 14:12:41 -0800

    made more changes to ui

--- src/Pass1.tsx
+++ src/Pass1.tsx
@@ -1,29 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { PassingContext } from './TechniqueContext';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
+
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { Button, responsiveFontSizes } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import { Link } from 'react-router-dom';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box'
+import { useMeQuery, usePostToLogMutation } from './generated/graphql';
 
 
 interface Props {
 
 }
+
+const labels: { [index: string]: string } = {
+  0.5: 'Useless',
+  1: 'Useless+',
+  1.5: 'Poor',
+  2: 'Poor+',
+  2.5: 'Ok',
+  3: 'Ok+',
+  3.5: 'Good',
+  4: 'Good+',
+  4.5: 'Excellent',
+  5: 'Excellent+',
+};
+
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -46,7 +60,7 @@ const useStyles = makeStyles((theme: Theme) =>
         width: 150,
         borderRadius: 20,
         fontSize: 20,
-        background: "purple",
+        background: "#8BE9FD",
         marginTop: 20,
         marginBottom: 20,
         '&:hover': {
@@ -54,28 +68,57 @@ const useStyles = makeStyles((theme: Theme) =>
     },  
     },
     header: {
-        backgroundColor: "blue",
+        backgroundColor: "#282A36",
         color: "white",
         fontSize: 100,
 
     },
     background: {
-        background: "blue",
+        background: "#282A36",
+    },
+    rating: {
+      width: 200,
+      display: 'flex',
+      alignItems: 'center'
     }
   }),
 );
 
 export const Pass1: React.FC<Props> = () => {
+
+  const pass1 = useContext(PassingContext)
+
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+
+
+  const [value, setValue] = React.useState<number | null>(2);
+  const [hover, setHover] = React.useState(-1);
+
+
+  const [postToUser] = usePostToLogMutation();
+  const [title, setTitle] = useState<any>('');
+  const [category, setCategory] = useState<any>('');
+  const [rank, setRank] = useState<any>('');
+  const [user_id, setUser_id] = useState<any>('');
+
+  const me = useMeQuery();
+  console.log(me.data?.me?.email)
+
+  function log() {
+    setTitle(pass1?.title)
+    setCategory(pass1?.category)
+    setRank(pass1?.rank)
+    setUser_id(me.data?.me?.email)
+    alert('if you log a new workout will be generated and this will be saved to your workout')
+  }
+
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
 
-  
-    const pass1 = useContext(PassingContext)
 
         return (
             <>
@@ -107,12 +150,21 @@ export const Pass1: React.FC<Props> = () => {
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
+         <div className={classes.root}>
+           <p>rate this technique (this will help us improve your daily workout)</p>
+      <Rating
+        name="hover-feedback"
+        value={value}
+        precision={0.5}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+      />
+      {value !== null && <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>}
+    </div>
         <IconButton
           className={clsx(classes.expand, {
             [classes.expandOpen]: expanded,
@@ -151,10 +203,27 @@ export const Pass1: React.FC<Props> = () => {
           </Typography>
         </CardContent>
       </Collapse>
+      <form
+        onSubmit={async e => {
+          e.preventDefault()
+          const response = await postToUser({
+            variables: {
+              title,
+              category,
+              rank,
+              user_id
+            }
+          })
+        }}
+        >
+        <Button className={classes.button} type='submit' onClick={log}>log</Button>
+
+        </form>
     </Card>
     </div>
+
             
-            </>
+  </>
 
         );
 }
